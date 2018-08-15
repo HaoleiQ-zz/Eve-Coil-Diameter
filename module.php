4affb9f5d971bd3ee4a955e85080f4ab8d0e8007
unknown <haoleiqin@qq.com>
2018-08-15 14:42:15 +0800

    增加前端 小程序端源码下载

--- module.php
+++ module.php
@@ -3,38 +3,53 @@
  *
  *
  * @author Haoleiqin
- * @url 
+ * @url
  */
 defined('IN_IA') or exit('Access Denied');
 
-class Init{
-	private function session()
+class Init
+{
+    private function session()
     {
         @session_start();
         $_SESSION['mininote_wechat'] = [
             'wxapp' => [
-                'wxapp_id' => $this->wechat_app['uniacid']
+                'wxapp_id' => $this->wechat_app['uniacid'],
             ],
             'we7_data' => [
                 'wxapp_id' => $this->wechat_app['uniacid'],
                 'app_name' => $this->wechat_app['name'],
                 'app_id' => $this->wechat_app['key'],
-                'app_secret' => $this->wechat_app['secret'],
+                'app_secret' => $this->wechat_app['secret'], 
             ],
-            'is_login' => true
-		];
-		
-		global $_W;
+            'is_login' => true,
+        ];
+
+        global $_W;
         $backEndUrl = "{$_W['siteroot']}addons/{$_W['current_module']['name']}/web/backEnd/public/index.php";
-        $frontEndUrl="{$_W['siteroot']}addons/{$_W['current_module']['name']}/web/frontEnd/";
-        header('Location:' . $frontEndUrl);
-        exit;
-	}
-	
-	public function execute(){
-      $this->session();
-	}
+        $frontEndUrl = "{$_W['siteroot']}addons/{$_W['current_module']['name']}/web/frontEnd/";
+
+        
+        $frontEndUrlWx = "{$_W['siteroot']}addons/{$_W['current_module']['name']}/web/wx/";
+        $frontEndUrlWxAccount = "{$_W['siteroot']}addons/{$_W['current_module']['name']}/web/wxaccount/";
+
+        if ($_W['account']['type'] == 4) {
+            header('Location:' . $frontEndUrlWx);
+            exit;
+        }else if($_W['account']['type'] == 1){
+            header('Location:' . $frontEndUrlWx);
+            exit;
+        } else {
+            header('Location:' . $frontEndUrl);
+            exit;
+        }
+
+    }
+
+    public function execute()
+    {
+        $this->session();
+    }
 }
 
 (new Init)->execute();
-
